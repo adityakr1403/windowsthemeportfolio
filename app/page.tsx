b9d75a19a8b4c3900381678e2a0c3f2bf776845c
aditya <adityakr1403@gmail.com>
2023-07-13 20:12:38 +0530

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import WindowSimple from "@/components/windows/window-simple";
+import Link from "next/link";
+import { FaGithubAlt, FaLinkedinIn } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+import { LiaHackerrank } from "react-icons/lia";
 
 export default function Home() {
   return (
@@ -7,10 +12,40 @@ export default function Home() {
         <div className="h-full w-full flex flex-col md:flex-row gap-5 justify-center items-center">
           <div className="avatar w-[200px] h-[200px] rounded-full backdrop-blur-3xl bg-blue-800"></div>
           <div className="details text-gray-100">
-            <h1>ADITYA KUMAR</h1>
-            <h1>ADITYA KUMAR</h1>
-            <h1>ADITYA KUMAR</h1>
-            <h1>ADITYA KUMAR</h1>
+            <h1 className="text-3xl">Aditya Kumar</h1>
+            <Link href={"adityakr1403@gmail.com"} type="email">
+              <code className="text-lg text-gray-300">adityakr1403@gmail.com</code>
+            </Link>
+            <div className=" grid grid-cols-2 gap-2 p-2 mt-3">
+              <Link
+                className="rounded-lg w-20 h-20 p-2 flex justify-center items-center hover:bg-[rgba(255,255,255,0.3)] backdrop-blur-3xl"
+                href={"https://github.com/adityakr1403"}
+                target="_blank"
+              >
+                <FaGithubAlt className=" text-[80px] text-black" />
+              </Link>
+              <Link
+                className="rounded-lg w-20 h-20 p-2 flex justify-center items-center hover:bg-[rgba(255,255,255,0.3)] backdrop-blur-3xl"
+                href={"https://www.linkedin.com/in/aditya-kumar-a77390283/"}
+                target="_blank"
+              >
+                <FaLinkedinIn className=" text-[80px] text-blue-500" />
+              </Link>
+              <Link
+                className="rounded-lg w-20 h-20 p-2 flex justify-center items-center hover:bg-[rgba(255,255,255,0.3)] backdrop-blur-3xl"
+                href={"https://leetcode.com/adityakr1403/"}
+                target="_blank"
+              >
+                <SiLeetcode className=" text-[80px] text-orange-600" />
+              </Link>
+              <Link
+                className="rounded-lg w-20 h-20 p-2 flex justify-center items-center hover:bg-[rgba(255,255,255,0.3)] backdrop-blur-3xl"
+                href={"https://www.hackerrank.com/adityakr1403?hr_r=1"}
+                target="_blank"
+              >
+                <LiaHackerrank className=" text-[80px] text-green-500" />
+              </Link>
+            </div>
           </div>
         </div>
       </WindowSimple>
